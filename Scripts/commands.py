e180fef8979d065b3ad6fa36c6ccb9005000842f
Boris Schwindt <boris.schwindt33@gmail.com>
2026-06-09 15:35:41 -0300

    Added new test method list sims, new export

--- Scripts/commands.py
+++ Scripts/commands.py
@@ -1,6 +1,15 @@
 import sims4.commands
+import services
+
 
 @sims4.commands.Command('simulacra.test', command_type=sims4.commands.CommandType.Live)
-def simulacra_test2(_connection=None):
+def simulacra_test(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
     output("Simulacra  is working!")
+
+@sims4.commands.Command('simulacra.list_sims', command_type=sims4.commands.CommandType.Live)
+def simulacra_list_sims(_connection=None):
+    sim_info_manager = services.sim_info_manager()
+    all_sims_indexed = sim_info_manager.get_all()
+    output = sims4.commands.CheatOutput(_connection)
+    output(f"All sims : {len(all_sims_indexed)}")
