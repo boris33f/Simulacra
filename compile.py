fb0cac8733eddec153e11b6f262842bd2b466dc2
Boris Schwindt <boris.schwindt33@gmail.com>
2026-06-09 17:28:46 -0300

    Updated compile script for clarity during usage

--- compile.py
+++ compile.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from Utilities import compile_module
 from settings import *
 
 root = os.path.dirname(__file__)
 compile_module(creator_name, root, mods_folder)
+print(datetime.now())
